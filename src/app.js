d09c33084b42968fd48fce1256ba5c960766f2d1
blaise <blaisehorvath@gmail.com>
2016-02-09 16:57:43 +0100

    Setting hardware address.

--- src/app.js
+++ src/app.js
@@ -3,21 +3,26 @@ let config = {
   baudrate: 115200
 };
 
+/*Importing the serialnode module*/
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort; // localize object constructor
 
+/*Initializing a serialnode object with the parameters*/
 var sp = new SerialPort( config.port , {
     parser: serialport.parsers.readline("\n"),
     baudrate: config.baudrate
 });
 
+/*Opening the serialport*/
 sp.open((err)=>{
     "use strict";
-    if (err) console.log(err)
+    if (err) console.log(err);
     else {
         console.log("Opened slot: ", config);
     }
-    sp.write("ifconfig\n", function(err, results) {
+
+    let address = new Promise( (resolve, reject) => {
+        sp.write("ifconfig\n", function(err, results) {
             if (err) console.log('err ' + err);
             //console.log('results ' + results);
             sp.on('data', function(data) {
@@ -27,35 +32,55 @@ sp.open((err)=>{
                     return item.trim()
                 });
                 let line = data.toString("utf8");
-                if (line.match(/(([a-zA-Z0-9]{4})::)([a-zA-Z0-9]{4}:){3}[a-zA-Z0-9]{4}/)) {
-                    let id = line.match(/(([a-zA-Z0-9]{4})::)([a-zA-Z0-9]{4}:){3}[a-zA-Z0-9]{4}/)[0];
-                    console.log(id);
+                let regexMatch = line.match(/(([a-zA-Z0-9]{4})::)([a-zA-Z0-9]{4}:){3}[a-zA-Z0-9]{4}/);
+                if (regexMatch) {
+                    let id = regexMatch[0];
+                    console.log("The address of the device is: " ,id);
+                    sp.close( (err)=>{
+                        if (err) console.log(err);
+                        else {
+                            console.log("Recieved data, closing connection!");
+                            resolve({ address: id });
+                        }
+                    })
                 }
             });
         });
-})
+    }).then((hardwareAddr) => {
 
-//var SerialPort = require("serialport").SerialPort;
-//var serialPort = new SerialPort(config.port, {
-//    baudrate: config.baudrate
-//}, false); // this is the openImmediately flag [default is true]
+            sp.open((err)=> {
+                if (err) console.log(err);
+                else {
+                    console.log("Opened slot: ", config);
+                    let lenght = hardwareAddr.address.length;
+                    hardwareAddr.id = hardwareAddr.address.substring(lenght - 2, lenght);
+                    console.log("Hardware addr is: ", hardwareAddr.id);
 
-//serialPort.open(function (error) {
-//    if ( error ) {
-//        console.log('failed to open: '+error);
-//    } else {
-//        console.log('The serial port is open:', config);
-//        serialPort.write("ifconfig\n", function(err, results) {
-//            if (err) console.log('err ' + err);
-//            //console.log('results ' + results);
-//            serialPort.on('data', function(data) {
-//                //process.stdout.write(data.toString("utf8"));
-//                let arr = data.toString("utf8").split(/\n|\r\n/);
-//                arr = arr.map((item) => {
-//                    return item.trim()
-//                });
-//                console.log(arr);
-//            });
-//        });
-//    }
-//});
+                    let output = "ifconfig 7 set addr " + hardwareAddr.id+"\n";
+
+                    sp.write(output, (err, res) => {
+                        if (err) {
+                            console.log(err);
+                        }
+                        else {
+                            console.log("response: " + res);
+                            sp.on("data", function (data) {
+                                let arr = data.toString("utf8").split(/\n|\r\n/);
+                                arr = arr.map((item) => {
+                                    return item.trim()
+                                });
+                                let line = data.toString("utf8");
+                                console.log(line);
+                            });
+                        };
+                    });
+                }
+
+
+            })
+
+
+
+    })
+
+});
