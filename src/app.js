30e2f4286168a5e8bdef6b5a556cdb23395c9d08
ViktorVaczi90 <viktor.vaczi.90@gmail.com>
2016-02-14 14:31:57 +0100

    working initialization with sm

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
-let config = {
-    port: "/dev/ttyACM1",
+"use strict"
+let serconf = {
+    port: "/dev/ttyUSB2",
     baudrate: 115200
 };
 
@@ -7,116 +8,134 @@ let config = {
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort; // localize object constructor
 
+var ifconfig_addr;
+
+/* Importing state maschine */
+
+let sm = require("state-machine-js");
 /*Initializing a serialnode object with the parameters*/
-var sp = new SerialPort(config.port, {
-    baudrate: config.baudrate
+var sp = new SerialPort(serconf.port, {
+    baudrate: serconf.baudrate
 });
+/* Initiating a buffer that will hold the data that's being streamed from the binary buffer */
+let buffer = [];
+/* Listening for the data event, and reading from the binary buffer
+ and filling up the string buffer until the end of the message */
+sp.on("data", (data)=> {
 
-/* Opening the serial port */
-sp.open((err) => {
-    if (err) console.log("Error initiating the connection");
-    else {
+    /* Converting the binary data to string and adding it to the string buffer */
+    buffer.push(data.toString("utf8"));
 
-        /* Starting the root configuration */
+    /* If the message is over, return the buffer  */
+    if (data.toString("utf8").match(">")) {
+        //serialPort.removeAllListeners();
+        let result = buffer.join('');
+        console.log(result);
+        if (result.match(/All up, running the shell now/)) {
+            stateMachine.action(Action.INITIATED);
+            buffer = [];
+        }
+        if (result.match(/inet6 addr: fe80::2e00:2500:1257:3346\/64  scope: local/)) {//TODO
+            ifconfig_addr = "fe80::2e00:2500:1257:3346";
+            stateMachine.action(Action.GOT_IPADDR);
+            buffer = [];
+        }
+        if (result.match(/success: added fe80::01\/64 to interface 7/)){
+            stateMachine.action(Action.SETNETIF_RDY);
+            buffer = [];
+        }
+        if (result.match(/successfully added a new RPL DODAG/)){
+            stateMachine.action(Action.DODAG_RDY);
+            buffer = [];
+        }
 
-        sendCommand(sp, "reboot").then(() => {
-            return sendCommand(sp, "ifconfig")
-        }).then( (data) => {
-
-            /* Extracting the hardware address from the inet6 address */
-
-            for (let item of data){
-                let match = item.match(/(([a-zA-Z0-9]{4})::)([a-zA-Z0-9]{4}:){3}[a-zA-Z0-9]{4}/);
-                if (match != null) {
-                    // the last 2 numbers of the address is the hardware address
-                    return match[0].slice(-2);
-                }
-            }
-
-        }).then( (hardwareAddress) => {
-
-            /* Setting the hardware address to the correct value */
-
-            return sendCommand(sp, "ifconfig 7 set addr " + hardwareAddress);
-
-        }).then(()=>{
-
-            /* Setting the rpl root adress */
-            return sendCommand(sp, "ifconfig 7 add 2001:db8::1")
-
-        }).then(() => {
-
-            /* Initiating the rpl */
-           return sendCommand(sp, "rpl init 7")
-
-        }).then(() => {
-
-           return sendCommand(sp, "rpl root 1 2001:db8::1")
-
-        }).then( (data) => {
-
-            /* Configuration completed closing the connection */
-            sp.close( () => {
-                console.log("Connection closed!")
-            })
-        })
     }
 });
 
-/**
- * A function that executes a RIOT shell commaand and returns the results of the command.
- * @param {Object} serialPort - A serialnode object.
- * @param {string} command - A valid RIOT os shell command
- * @returns {*|Promise|P} A promise for array of utf-8 strings that was converted from the buffer's data.
- */
-let sendCommand = function (serialPort, command) {
+sp.open( () => {
+    // start
+    stateMachine.start();
+});
+var stateMachine = new sm();
 
-    /* adding a \n to the end of the command */
-    if (command.endsWith('\n')  == false) command = command + "\n";
-
-    return new Promise( (resolve, reject) => {
-
-        /* Writing the command to the serial */
-        serialPort.write(command, (err, res) => {
-            if (err) console.log(err);
-            else {
-
-                /* Initiating a buffer that will hold the data that's being streamed from the binary buffer */
-                let buffer = [];
-
-                /* Listening for the data event, and reading from the binary buffer
-                 and filling up the string buffer until the end of the message */
-                serialPort.on("data", (data)=> {
-
-                    /* Converting the binary data to string and adding it to the string buffer */
-                    buffer.push(data.toString("utf8"));
-
-                    /* If the message is over, return the buffer  */
-                    if (data.toString("utf8").match(">")) {
-                        serialPort.removeAllListeners();
-                        let result = buffer.join(' ');
-                        console.log(result);
-                        resolve(result.split('\n').map((item)=>{ return item.trim() }));
-                    }
-                })
-            }
-        })
-    });
+var State = {
+    INTIAL: 'INTIAL',
+    REBOOT: 'REBOOT',
+    GETNETIF: 'GETNETIF',
+    SETNETIF: 'SETNETIF',
+    SETNETIF2: 'SETNETIF2',
+    READY: 'READY'
 };
 
-
-let initiateRpl = function (serialPort) {
-
-
+var Action = {
+    INITIATED: 'INITIATED',
+    GOT_IPADDR: 'GOT_IPADDR',
+    SETNETIF_RDY: 'SETNETIF_RDY',
+    DODAG_RDY: 'DODAG_RDY'
 };
+var config = [
+    {
+        initial: true,
+        name: State.INTIAL,
+        transitions: [
+            { action: Action.INITIATED, target: State.GETNETIF }
+        ],
+        onEnter: function(state,data,action){
+            sp.write("reboot\n");
+        }
+    },
+    {
+        name: State.REBOOT,
+        transitions: [
+            { action: Action.INITIATED, target: State.GETNETIF }
+        ],
+        onEnter: function(state,data,action){
+            sp.write("reboot\n");
+        }
+    },
+    {
+        name: State.GETNETIF,
+        transitions: [
+            { action: Action.GOT_IPADDR, target: State.SETNETIF }
+        ],
+        onEnter: function(state,data,action){
+            sp.write("ifconfig\n");
+        }
+    },
+    {
+        name: State.SETNETIF,
+        transitions: [
+            { action: Action.SETNETIF_RDY, target: State.SETNETIF2 }
+        ]
+        ,
+        onEnter: function(state,data,action){
+            sp.write("ifconfig 7 del "+ifconfig_addr + "\n" +
+            "ifconfig 7 set addr 01\n"+
+            "ifconfig 7 add fe80::01\n");
 
-//let initiateRpl = function (serialPort) {
-//
-//    return new Promise((resolve, reject) => {
-//        sendCommand(serialPort, "rpl init 7").then((answer) => {
-//            sendCommand(serialPort, "rpl root 1 2001:db8::1").then( (answer) => {
-//                resolve(answer);
-//            })
-//        })
-//    });
-//};
+        }
+    },
+    {
+        name: State.SETNETIF2,
+        transitions: [
+            { action: Action.DODAG_RDY, target: State.READY }
+        ]
+        ,
+        onEnter: function(state,data,action){
+            sp.write("ifconfig 7 add 2001:db8::1\n"+
+            "rpl init 7\n"+
+            "rpl root 1 2001:db8::1\n");
+        }
+    }
+];
+
+// create multiple states with a config array
+stateMachine.create(config);
+
+// add listener for state change
+stateMachine.onChange.add(function(state, data, action) {
+    console.log('State has changed to:', state.name);
+    console.log('Got data:', data);
+    console.log('Got triggering action:', action);
+});
+
---
+++ src/app2.js
@@ -0,0 +1,122 @@
+let config = {
+    port: "/dev/ttyUSB1",
+    baudrate: 115200
+};
+
+/* Importing the serialnode module */
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort; // localize object constructor
+
+/*Initializing a serialnode object with the parameters*/
+var sp = new SerialPort(config.port, {
+    baudrate: config.baudrate
+});
+
+/* Opening the serial port */
+sp.open((err) => {
+    if (err) console.log("Error initiating the connection");
+else {
+
+    /* Starting the root configuration */
+
+    sendCommand(sp, "reboot").then(() => {
+        return sendCommand(sp, "ifconfig")
+    }).then( (data) => {
+
+        /* Extracting the hardware address from the inet6 address */
+
+        for (let item of data){
+        let match = item.match(/(([a-zA-Z0-9]{4})::)([a-zA-Z0-9]{4}:){3}[a-zA-Z0-9]{4}/);
+        if (match != null) {
+            // the last 2 numbers of the address is the hardware address
+            return match[0].slice(-2);
+        }
+    }
+
+}).then( (hardwareAddress) => {
+
+    /* Setting the hardware address to the correct value */
+
+    return sendCommand(sp, "ifconfig 7 set addr " + hardwareAddress);
+
+}).then(()=>{
+
+    /* Setting the rpl root adress */
+    return sendCommand(sp, "ifconfig 7 add 2001:db8::1")
+
+}).then(() => {
+
+    /* Initiating the rpl */
+    return sendCommand(sp, "rpl init 7")
+
+}).then(() => {
+
+    return sendCommand(sp, "rpl root 1 2001:db8::1")
+
+}).then( (data) => {
+
+    /* Configuration completed closing the connection */
+    sp.close( () => {
+    console.log("Connection closed!")
+})
+})
+}
+});
+
+/**
+ * A function that executes a RIOT shell commaand and returns the results of the command.
+ * @param {Object} serialPort - A serialnode object.
+ * @param {string} command - A valid RIOT os shell command
+ * @returns {*|Promise|P} A promise for array of utf-8 strings that was converted from the buffer's data.
+ */
+let sendCommand = function (serialPort, command) {
+
+    /* adding a \n to the end of the command */
+    if (command.endsWith('\n')  == false) command = command + "\n";
+
+    return new Promise( (resolve, reject) => {
+
+            /* Writing the command to the serial */
+            serialPort.write(command, (err, res) => {
+            if (err) console.log(err);
+    else {
+
+        /* Initiating a buffer that will hold the data that's being streamed from the binary buffer */
+        let buffer = [];
+
+        /* Listening for the data event, and reading from the binary buffer
+         and filling up the string buffer until the end of the message */
+        serialPort.on("data", (data)=> {
+
+            /* Converting the binary data to string and adding it to the string buffer */
+            buffer.push(data.toString("utf8"));
+
+        /* If the message is over, return the buffer  */
+        if (data.toString("utf8").match(">")) {
+            serialPort.removeAllListeners();
+            let result = buffer.join('');
+            console.log(result);
+            resolve(result.split('\n').map((item)=>{ return item.trim() }));
+        }
+    })
+}
+})
+});
+};
+
+
+let initiateRpl = function (serialPort) {
+
+
+};
+
+//let initiateRpl = function (serialPort) {
+//
+//    return new Promise((resolve, reject) => {
+//        sendCommand(serialPort, "rpl init 7").then((answer) => {
+//            sendCommand(serialPort, "rpl root 1 2001:db8::1").then( (answer) => {
+//                resolve(answer);
+//            })
+//        })
+//    });
+//};
