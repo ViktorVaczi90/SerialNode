5e9ac1dd7bc814169a2ef3396145e901a4d68447
blaise <blaisehorvath@gmail.com>
2016-03-21 17:28:06 +0100

    Opening connection

--- src/example.js
+++ src/example.js
@@ -2,20 +2,14 @@
 /**
  * Created by v on 2016.02.29..
  */
-const SerialHandler = require("./serial2.js");
+const SerialHandler = require("./serial.js");
 
-let prom = new Promise((res, rej)=>{
-    let serial = new SerialHandler();
-    serial.sp.open((err)=>{
-        res(serial)
-    });
-});
+let serial = new SerialHandler();
 
-prom.then( (serial) =>{
+serial.openConn().then(()=>{
         serial.handleSerial("reboot\n").then((data)=>{
-            console.log(data)
-        }).then(()=>{
-            serial.closeConn();
-        });
-}
-);
+        console.log(data)
+    }).then(()=>{
+        serial.closeConn();
+    });
+})
