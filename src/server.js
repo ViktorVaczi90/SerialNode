8bfc10335b42fa696919d4d237c4f73729e6e4bc
blaise <blaisehorvath@gmail.com>
2016-03-22 21:13:43 +0100

    Posting data to server is working.

--- src/server.js
+++ src/server.js
@@ -2,16 +2,34 @@
 const express = require('express');
 const db = require("./db");
 
+
 const app = express();
-const appDatas = require('./app.js');
-let datas = new appDatas;
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
+//const appDatas = require('./app.js');
+//let datas = new appDatas;
 
 app.get('/', (req, res) => {
-    res.sendfile("./index.html");
+    res.sendFile("index.html", {"root": __dirname});
 });
-app.get('/leds',(req,res) => {
-    res.send(datas.getData());
-})
+
+//app.get('/leds',(req,res) => {
+//    res.send(datas.getData());
+//})
+
+let i = 1;
+app.post('/leds', (req, res) => {
+    console.log("POST", "/leds", ++i, req.body);
+    console.log();
+    res.statusCode = 200;
+    res.send()
+});
+
 app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
+    console.log('SerialNode server is listening on port 3000!');
 });
